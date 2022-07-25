b79862d1d446920f65f004989c4aafb08f48cdb0
DESKTOP-PLL311C\youngseok <mana129@douzone.com>
2022-07-25 09:28:42 +0900

    근태동의 부분 수정 및 작업스케줄러생성bat 수정

--- create_scheduler.bat
+++ create_scheduler.bat
@@ -2,6 +2,6 @@
 @echo 작업스케줄러에 등록합니다
 @echo ####################################
 @echo ##java -jar 
-schtasks /create /tn "스케줄러실행" /tr javaw -jar C:\batchFile\scheduler.jar /sc ONSTART
+schtasks /create /tn "자동스케줄러시작" /tr C:\batchFile\scheduler.bat /sc ONLOGON /RL HIGHEST
 
 pause
