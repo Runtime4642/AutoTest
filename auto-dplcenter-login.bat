28fc3ffc9f3f9f8f547a3f395c7f643ef65f4555
DESKTOP-PLL311C\youngseok <mana129@douzone.com>
2022-08-02 10:19:40 +0900

    스케줄러 등록 옵션 수정 및 visibleDPL , 로깅 처리 추가

---
+++ auto-dplcenter-login.bat
@@ -0,0 +1,8 @@
+@echo ####################################
+@echo 일일보고 프로세스
+@echo ####################################
+@echo ##java -jar 
+cd C:\batchFile
+java -jar C:\batchFile\auto-dplcenter-login.jar
+
+timeout 20
